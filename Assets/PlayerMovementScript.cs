eda06e38fdab0838d102e531a63a1e8c48451a69
Sebastian Snyder <sebastiansnyder@yahoo.com>
2018-01-26 22:04:00 -0600

    Did the collision and stuff

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+	private CharacterController Controller;
+	private float MoveSpeed = 0.3f;
+
+	// Use this for initialization
+	void Start()
+	{
+		Controller = (CharacterController)GetComponent(typeof(CharacterController));
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 Dir = Vector3.zero;
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			Dir.z += MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			Dir.z -= MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			Dir.x -= MoveSpeed;
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			Dir.x += MoveSpeed;
+		}
+		Controller.Move( Vector3.ClampMagnitude(Dir, MoveSpeed) );
+	}
+}
