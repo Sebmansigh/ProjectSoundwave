8f4f239f2db03c50b14b200f1a553c0e64fcc0c4
Sebastian Snyder <sebastiansnyder@yahoo.com>
2018-01-27 23:18:12 -0600

    Doors Partially Coded

---
+++ Assets/DoorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+	public GameObject m_DoorLeft;
+	public GameObject m_DoorRight;
+
+	bool triggered = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Vector3 Left = m_DoorRight.GetComponent<Rigidbody>().position - m_DoorLeft.GetComponent<Rigidbody>().position;
+		print(m_DoorRight.GetComponent<Rigidbody>().position + " - " + m_DoorLeft.GetComponent<Rigidbody>().position);
+		m_DoorLeft.AddComponent<LeftDoorBehavior>().Left = Left;
+		m_DoorRight.AddComponent<RightDoorBehavior>().Left = Left;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnSoundTrigger () 
+	{
+		if(!triggered)
+		{
+			m_DoorLeft.SendMessage("SoundActivate");
+			m_DoorRight.SendMessage("SoundActivate");
+			triggered = true;
+		}
+	}
+}
---
+++ Assets/LeftDoorBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftDoorBehavior : MonoBehaviour
+{
+	private bool Triggered = false;
+	private Vector3 InitialPosition;
+
+	public Vector3 Left;
+
+	private float AmtTraveled;
+	// Use this for initialization
+	void Start ()
+	{
+		InitialPosition = transform.position;
+		AmtTraveled = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Triggered)
+		{
+			if(AmtTraveled > 7)
+			{
+				//GetComponent<Rigidbody>().velocity = Vector3.zero;
+				//Destroy(this); //Yes, removing this script component is the desired behavior.
+				Triggered = false;
+				print("click!");
+			}
+			else
+			{
+				//GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 5.0f);
+				transform.position += Left * 0.1f;
+				AmtTraveled += Left.magnitude * 0.1f;
+				//print(InitialPosition.z - transform.position.z);
+				print(Left + " ->" + AmtTraveled);
+			}
+		}
+	}
+
+	void SoundActivate ()
+	{
+		Triggered = true;
+	}
+}
