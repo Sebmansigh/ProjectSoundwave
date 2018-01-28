5bdb249fe664af98b5b7c3e3c74f58a0e3ff1a75
Sebastian Snyder <sebastiansnyder@yahoo.com>
2018-01-28 14:34:04 -0600

    End of level is detected

---
+++ Assets/LevelEndTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter()
+	{
+		GameObject.Find("LevelEndText").GetComponent<MeshRenderer>().enabled = true;
+		Destroy(this);
+	}
+}
