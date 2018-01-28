07228c8f44c5e69e1b0cc5b65b5fcc5a14a50e6b
Sebastian Snyder <sebastiansnyder@yahoo.com>
2018-01-28 11:18:07 -0600

    Bars activate and are solid now.

---
+++ Assets/BarsTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarsTrigger : MonoBehaviour
+{
+	public GameObject m_Bars;
+
+	bool triggered = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Bars.AddComponent<BarsBehavior>();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnSoundTrigger () 
+	{
+		if(!triggered)
+		{
+			m_Bars.SendMessage("SoundActivate");
+			triggered = true;
+		}
+	}
+}
